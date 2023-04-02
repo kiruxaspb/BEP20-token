612c513c56064b6e13591f83d7bf955aa5603356
kiruxaspb <dvoreckovkirill@gmail.com>
2023-04-02 10:55:25 +0300

    Update code

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
-// Implementation OpenZeppelin Contracts (last updated v4.7.0) (access/Ownable.sol) for BEP20 standart
+// Implementation OpenZeppelin Contracts (last updated v4.7.0) (access/Ownable.sol) for BEP20 standard
+
 pragma solidity ^0.8.0;
 
 import "./Context.sol";
